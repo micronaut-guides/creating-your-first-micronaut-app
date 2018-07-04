209ee5142788593f9507fe13cbeb662407b857b4
sdelamo <sergio.delamo@softamo.com>
2018-07-04 17:17:28 +0200

    Set content-type to plain/text
    See: https://github.com/micronaut-guides/creating-your-first-micronaut-app-groovy/issues/1

--- complete/src/main/java/example/micronaut/HelloController.java
+++ complete/src/main/java/example/micronaut/HelloController.java
@@ -3,10 +3,13 @@ package example.micronaut;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 
+import java.awt.*;
+
 @Controller("/hello") // <1>
 public class HelloController {
     @Get("/") // <2>
+    @Produces(PageAttributes.MediaType.TEXT_PLAIN) // <3>
     public String index() {
-        return "Hello World"; // <3>
+        return "Hello World"; // <4>
     }
 }
