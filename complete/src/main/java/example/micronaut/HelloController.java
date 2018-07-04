d6f4332f78fad5421f728415d764957190189160
sdelamo <sergio.delamo@softamo.com>
2018-07-04 17:28:05 +0200

    Fixes @Produces import
    See: https://github.com/micronaut-guides/creating-your-first-micronaut-app-groovy/issues/1

--- complete/src/main/java/example/micronaut/HelloController.java
+++ complete/src/main/java/example/micronaut/HelloController.java
@@ -1,14 +1,14 @@
 package example.micronaut;
 
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-
-import java.awt.*;
+import io.micronaut.http.annotation.Produces;
 
 @Controller("/hello") // <1>
 public class HelloController {
     @Get("/") // <2>
-    @Produces(PageAttributes.MediaType.TEXT_PLAIN) // <3>
+    @Produces(MediaType.TEXT_PLAIN) // <3>
     public String index() {
         return "Hello World"; // <4>
     }
