80ffe93a4c057e567f0c08bf20344b452b30d02b
willoci <48927697+willoci@users.noreply.github.com>
2020-06-16 00:00:42 -0500

    update to mn 2.0 (#10)
    * update to mn 2.0
    
    * remove complete-orig folder
    
    * delete complete/.gitignore
    
    * Delete CompleteTest.java
    
    It is a generated file by Micronaut Launch
    
    * Update to Gradle 6.5
    
    * move HelloControllerTest to example.micronaut package
    
    * link to micronaut 1.x version of this guide
    
    * build: remove .gitkeep and CompleteTest
    
    * Update graalvm section
    
    Co-authored-by: Sergio del Amo <sergio.delamo@softamo.com>

--- complete/docker-build.sh
+++ complete/docker-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-docker build . -t micronautguide
+docker build . -t complete
 echo
 echo
 echo "To run the docker container execute:"
-echo "    $ docker run -p 8080:8080 micronautguide"
+echo "    $ docker run -p 8080:8080 complete"
